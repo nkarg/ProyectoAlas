f08163c79875c558c333d0fba227b95072cf339d
Eduardo Rojo <r.cadenas.edu@gmail.com>
2019-01-07 11:47:31 -0300

    Carpetas

---
+++ Proyecto Alas/Core/Infraestructure/Custom/ResultEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Infraestructure.Custom
+{
+    public class ResultEntity
+    {
+        public bool ResultOk { get; set; }
+        public object BusinessEntity { get; set; }
+        public string Message { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+    }
+}
